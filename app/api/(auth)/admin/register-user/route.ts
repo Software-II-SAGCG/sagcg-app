ae726de4ed86cd6a7721e9a48c51515de022f5de
KevinJOH <13-11007@usb.ve>
2025-02-19 11:56:00 -0400

    Add admin endpoints

---
+++ app/api/(auth)/admin/register-user/route.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { NextRequest, NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.text();
+    if (!body) {
+      return new NextResponse(JSON.stringify({ error: "El cuerpo de la solicitud está vacío" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const { username, nombre, apellido, email, password, rolid } = JSON.parse(body); 
+
+    if (!username || !nombre || !apellido || !email || !password || !rolid) {
+      return new NextResponse(JSON.stringify({ error: "Todos los campos son obligatorios" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const usuario = await prisma.usuario.create({
+      data: {
+        username,
+        nombre,
+        apellido,
+        email,
+        password: hashedPassword,
+        emailVerified: false,
+        rolid: Number(rolid),
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ message: "Usuario registrado con éxito", usuario }), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+
+  } catch (error: any) {
+    console.error(error);
+
+    let status = 500;
+    let message = "Error al registrar usuario";
+
+    if (error.code === "P2002") {
+      status = 400;
+      message = "El email o username ya existen.";
+    } else if (error.name === "PrismaClientKnownRequestError") {
+      message = "Error en la base de datos.";
+    }
+
+    return new NextResponse(JSON.stringify({ error: message }), {
+      status,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
+
