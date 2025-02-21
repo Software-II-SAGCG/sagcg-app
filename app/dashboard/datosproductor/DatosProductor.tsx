1e3fd6e5335e9ebb6a4e27c6247b4e0feee3e9bc
KevinJOH <13-11007@usb.ve>
2025-02-21 19:58:53 -0400

    Edit buttons

--- app/dashboard/datosproductor/DatosProductor.tsx
+++ app/dashboard/datosproductor/DatosProductor.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaTimes, FaEdit } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
 
 interface Producer {
   id: number;
@@ -312,18 +313,20 @@ export default function DatosProductor() {
                     {producerTypes.find((tp) => tp.id === producer.tipoid)
                       ?.nombre || producer.tipoid}
                   </td>
-                  <td className="border p-2 flex space-x-2">
+                  <td className="p-2 flex space-x-2 mt-2">
                     <button
                       onClick={() => openEditModal(producer)}
-                      className="bg-yellow-500 text-white px-4 py-2 rounded-lg shadow-lg"
+                      className="bg-yellow-400 text-black px-4 py-2 rounded-lg shadow-lg border border-yellow-500"
+                      title="Editar Productor"
                     >
-                      Editar
+                      <MdEdit size={24}/>
                     </button>
                     <button
                       onClick={() => handleDelete(producer)}
-                      className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-lg"
+                      className="bg-red-400 text-black px-4 py-2 rounded-lg shadow-lg border border-red-500"
+                      title="Eliminar Productor"
                     >
-                      Eliminar
+                      <FaTimes size={24}/>
                     </button>
                   </td>
                 </tr>
