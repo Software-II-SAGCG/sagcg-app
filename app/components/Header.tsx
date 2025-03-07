001651edc6798ca77a6d225b33403a479145b51f
KevinJOH <13-11007@usb.ve>
2025-03-07 00:13:20 -0400

    Fix Header Component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -28,15 +28,17 @@ const Header: React.FC<HeaderProps> = ({
       </div>
 
       {/* Search and Add Buttons */}
-      {showSearchBar && (
+      {(showSearchBar || showSearchButton || showAddButton) && (
         <div className="flex justify-between my-4">
-          <input
-            type="text"
-            placeholder="Buscar usuario..."
-            value={onSearchChange ? searchValue : undefined}
-            onChange={onSearchChange}
-            className="border p-2 rounded w-full max-w-md text-gray-900"
-          />
+          {(showSearchBar &&
+            <input
+              type="text"
+              placeholder="Buscar usuario..."
+              value={onSearchChange ? searchValue : undefined}
+              onChange={onSearchChange}
+              className="border p-2 rounded w-full max-w-md text-gray-900"
+            />
+          )}
           <div className="flex gap-2">
             {showSearchButton && (
               <button 
